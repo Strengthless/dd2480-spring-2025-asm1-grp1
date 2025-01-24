b7578816a2af69cac1baf05fedd5b950681a8e74
Marcello Krahforst <marcello.2001@hotmail.com>
2025-01-24 15:49:21 +0100

    feat(#10): Implement get_pum function

--- modules/pum.py
+++ modules/pum.py
@@ -1,5 +1,20 @@
 from modules.types import Connectors
 
 def get_pum(cmv: list[int], lcm: list[list[Connectors]]) -> list[list[bool]]:
-    # TODO: To be implemented
-    return [[False] * 15] * 15
+    # create pum matrix with true as default
+    n = len(cmv) 
+    pum = [[True] * n] * n
+
+    # go over matrix (only half because symmetric)
+    for i in range(n):
+        for j in range(i):
+            # set pum according to operation in lcm
+            if lcm[i,j] == Connectors.ANDD:
+                pum[i,j] = cmv[i] and cmv[j]
+            elif lcm[i,j] == Connectors.ORR:
+                pum[i,j] = cmv[i] or cmv[j]
+            
+            # mirror values to other side
+            pum[j,i] = pum[i,j]
+
+    return pum
