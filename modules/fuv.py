09a3dbde786969939afe1dd31c8431b9615fc92c
Marcello Krahforst <marcello.2001@hotmail.com>
2025-01-25 13:27:00 +0100

    feat(#7): Implements get_fuv function
    This commit implements the get_fuv function thus completing the first
    part of the issue

--- modules/fuv.py
+++ modules/fuv.py
@@ -1,3 +1,14 @@
 def get_fuv(pum: list[list[bool]], puv: list[bool]) -> list[bool]:
-    # TODO: To be implemented
-    return [False] * 15
+    # check if inputs have correct size
+    assert(len(pum) == 15)
+    assert(len(pum[0]) == 15)
+    assert(len(puv) == 15)
+    
+    # initialize fuv with as all False
+    fuv = [False] * 15
+
+    for i in range(15):
+        # fuv is True if puv[i] is false or the entire row of pum[i] is True
+        fuv[i] = (not puv[i]) or (pum[i] == [True] * 15) 
+
+    return fuv
