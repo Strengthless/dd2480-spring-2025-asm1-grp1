fa65cb572992624425c0be5ba8a6f2475cf11264
HerodrawzZ <73788234+HerodrawzZ@users.noreply.github.com>
2025-01-26 14:02:31 +0100

    Added LIC 10 logic and test cases

--- modules/cmv.py
+++ modules/cmv.py
@@ -1,5 +1,43 @@
 from modules.types import Coordinate, Parameters
 
+import numpy as np
+
 def get_cmv(num_points: int, points: list[Coordinate], parameters: Parameters) -> list[bool]:
     # TODO: To be implemented
     return [False] * 15
+
+def check_lic_10(points, parameters):
+    """
+    Inputs:
+        points: List of coordinates of two-dimensional points
+        parameters: Dictionary contains the parameters for the LIC and CMV
+
+    Ouputs:
+        Boolean: True if the condition is met, False otherwise
+
+    """
+    
+    area1 = parameters["area1"]
+    e_pts = parameters["e_pts"]
+    f_pts = parameters["f_pts"]
+
+    if len(points) < 5:                         # The condition is not met when the number of points is less than 5
+        return False
+    
+    assert(e_pts >= 1)                          # e_pts must be greater than or equal to 1
+    assert(f_pts >= 1)                          # f_pts must be greater than or equal to 1
+    assert(e_pts + f_pts <= len(points) - 3)    # The sum of e_pts and f_pts must be less than or equal to the number of points - 3
+
+    for i in range(len(points)-e_pts-f_pts-2):
+
+        # Calculate the area of the triangle that is defined by the two vectors formed by the point i to the point i+e_pts+1 and i+f_pts+2 respectively
+
+        vector_1 = [points[i+e_pts+1][0] - points[i][0], points[i+e_pts+1][1] - points[i][1]]
+        vector_2 = [points[i+e_pts+f_pts+2][0] - points[i][0], points[i+e_pts+f_pts+2][1] - points[i][1]]
+
+        calc_area = abs((1/2)*np.cross(vector_1, vector_2))
+
+        if calc_area > area1:       # If the area is greater than the area1, the condition is met.
+            return True
+
+    return False
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 from modules.decide import determine_launch
+from modules.cmv import *
 
 class DecideTests(unittest.TestCase):
     def test_should_launch_if_fuv_is_all_true(self):
@@ -22,6 +23,51 @@ class CMVTests(unittest.TestCase):
     # TODO: Add actual tests here
     def test_something(self):
         self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_lic_10(self):
+
+        # Define the parameters for LIC 10 test
+
+        parameters = {
+            "area1": 2,
+            "e_pts": 3,
+            "f_pts": 4
+        }
+
+        points_1 = [(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0)]
+        points_2 = [(0,0),(0,0),(0,0),(0,0),(10,0),(0,0),(0,0),(0,0),(0,0),(0,10),(0,0),(0,0)]
+        points_3 = [(0,0),(0,0),(0,0),(0,0),(0,0),(0,1),(0,0),(0,0),(0,0),(0,0),(1,0),(0,0)]
+        points_4 = [(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(3,0),(0,0),(0,0),(0,0),(0,0),(3,3)]
+
+        # Test the LIC 10 logic
+
+        self.assertFalse(check_lic_10(points_1, parameters))
+        self.assertTrue(check_lic_10(points_2, parameters))
+        self.assertFalse(check_lic_10(points_3, parameters))
+        self.assertTrue(check_lic_10(points_4, parameters))
+
+        parameters["area1"] = 51
+        
+        self.assertFalse(check_lic_10(points_2, parameters))
+        self.assertFalse(check_lic_10(points_4, parameters))
+
+        # Testing if the built in assertions work
+
+        with self.assertRaises(AssertionError):
+            parameters["e_pts"] = 0
+            parameters["f_pts"] = 1
+            check_lic_10(points_1, parameters)
+        
+        with self.assertRaises(AssertionError):
+            parameters["e_pts"] = 1
+            parameters["f_pts"] = 0
+            check_lic_10(points_1, parameters)
+
+        with self.assertRaises(AssertionError):
+            parameters["e_pts"] = 1
+            parameters["f_pts"] = 3
+            points_5 = [(0,0),(0,0),(0,0),(0,0),(0,0),(0,0)]
+            check_lic_10(points_5, parameters)
 
 class FUVTest(unittest.TestCase):
     # TODO: Add actual tests here
