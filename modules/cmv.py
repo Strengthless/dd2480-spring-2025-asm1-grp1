f90402d64fad03c0a673db0b0661b8ab0d755afb
Johan Nilsson <ni5@ug.kth.se>
2025-01-26 10:30:57 +0100

    feat(#18): LIC5 is now regarded in CMV

--- modules/cmv.py
+++ modules/cmv.py
@@ -1,5 +1,12 @@
 from modules.types import Coordinate, Parameters
 
 def get_cmv(num_points: int, points: list[Coordinate], parameters: Parameters) -> list[bool]:
-    # TODO: To be implemented
-    return [False] * 15
+    CMV =  [False for i in range(15)]
+
+    CMV[5] = check_LIC5(num_points, points, parameters)
+
+    return CMV
+
+
+def check_LIC5(num_points: int, points: list[Coordinate], parameters: Parameters) -> bool:
+    return any(p2.x - p1.x < 0 for p1, p2 in zip(points, points[1:]))
