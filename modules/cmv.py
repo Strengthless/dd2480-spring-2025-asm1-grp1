6dc9338a5db42d625f134ddc2dd6e3ae2229e13f
Johan Nilsson <ni5@ug.kth.se>
2025-01-28 10:13:52 +0100

    test(#19): Added tests for LIC 6 and changed np.cross use, tests passed

--- modules/cmv.py
+++ modules/cmv.py
@@ -1,28 +1,36 @@
 from modules.types import Coordinate, Parameters
 import numpy as np
 
+
 # Helper functions
 def check_clic_1() -> bool:
     # TODO: Update the function signature and implementation
     return False
 
+
 def check_lic_2() -> bool:
     # TODO: Update the function signature and implementation
     return False
+
 
 def check_lic_3() -> bool:
     # TODO: Update the function signature and implementation
     return False
 
+
 def check_lic_4() -> bool:
     # TODO: Update the function signature and implementation
     return False
+
 
 def check_lic_5() -> bool:
     # TODO: Update the function signature and implementation
     return False
 
-def check_lic_6(num_points: int, points: list[Coordinate], parameters: Parameters) -> bool:
+
+def check_lic_6(
+    num_points: int, points: list[Coordinate], parameters: Parameters
+) -> bool:
     n_pts = parameters["n_pts"]
     dist = parameters["dist"]
 
@@ -30,39 +38,61 @@ def check_lic_6(num_points: int, points: list[Coordinate], parameters: Parameter
         return False
 
     for i in range(len(points) - n_pts + 1):
-        stripped_points = points[i:i + n_pts]
-        first_point = np.array([stripped_points[0]["x"], stripped_points[0]["y"]], dtype=float)
-        last_point = np.array([stripped_points[-1]["x"], stripped_points[-1]["y"]], dtype=float)
+        stripped_points = points[i : i + n_pts]
+        first_point = np.array(
+            [stripped_points[0]["x"], stripped_points[0]["y"]], dtype=float
+        )
+        last_point = np.array(
+            [stripped_points[-1]["x"], stripped_points[-1]["y"]], dtype=float
+        )
 
         if np.array_equal(first_point, last_point):
             for j in range(1, n_pts):
-                current_point = np.array([stripped_points[j]["x"], stripped_points[j]["y"]], dtype=float)
+                current_point = np.array(
+                    [stripped_points[j]["x"], stripped_points[j]["y"]], dtype=float
+                )
                 cur_pt_dist_to_pt = np.linalg.norm(first_point - current_point)
 
                 if cur_pt_dist_to_pt > dist:
                     return True
-            
+
         else:
             for j in range(1, n_pts):
-                current_point = np.array([stripped_points[j]["x"], stripped_points[j]["y"]], dtype=float)
-                cur_pt_dist_to_line = np.abs(np.cross(last_point - first_point, first_point - current_point)) / np.linalg.norm(last_point - first_point)
+                current_point = np.array(
+                    [stripped_points[j]["x"], stripped_points[j]["y"]], dtype=float
+                )
+                ax, ay = last_point[0] - first_point[0], last_point[1] - first_point[1]
+                bx, by = (
+                    first_point[0] - current_point[0],
+                    first_point[1] - current_point[1],
+                )
+
+                cross_value = ax * by - ay * bx
+
+                cur_pt_dist_to_line = np.abs(cross_value) / np.linalg.norm(
+                    last_point - first_point
+                )
 
                 if cur_pt_dist_to_line > dist:
                     return True
     return False
 
+
 def check_lic_7() -> bool:
     # TODO: Update the function signature and implementation
     return False
+
 
 def check_lic_8() -> bool:
     # TODO: Update the function signature and implementation
     return False
 
+
 def check_lic_9() -> bool:
     # TODO: Update the function signature and implementation
     return False
-  
+
+
 def check_lic_10(points: list[Coordinate], parameters: Parameters):
     """
     Function to check if the LIC 10 is satisfied
@@ -74,31 +104,31 @@ def check_lic_10(points: list[Coordinate], parameters: Parameters):
             dictionary containing the parameters for LIC and CMW
 
     Outputs:
-        Boolean 
+        Boolean
             True if the condition is met, False otherwise
 
     """
-    
+
     area1 = parameters["area1"]
     e_pts = parameters["e_pts"]
     f_pts = parameters["f_pts"]
 
-    if len(points) < 5:                         
+    if len(points) < 5:
         return False
-    
-    if (e_pts < 1):
-        raise ValueError        
-    if (f_pts < 1):
-        raise ValueError
-    if (e_pts + f_pts > len(points) - 3):
-        raise ValueError                        
 
-    for i in range(len(points)-e_pts-f_pts-2):
+    if e_pts < 1:
+        raise ValueError
+    if f_pts < 1:
+        raise ValueError
+    if e_pts + f_pts > len(points) - 3:
+        raise ValueError
+
+    for i in range(len(points) - e_pts - f_pts - 2):
 
         # Calculate the area of the triangle that is defined by the two vectors formed by the point i to the point j and k respectively
 
-        j = i+e_pts+1
-        k = i+e_pts+f_pts+2
+        j = i + e_pts + 1
+        k = i + e_pts + f_pts + 2
 
         coord_1 = points[i]
         coord_2 = points[j]
@@ -107,42 +137,50 @@ def check_lic_10(points: list[Coordinate], parameters: Parameters):
         vector_1 = [coord_2["x"] - coord_1["x"], coord_2["y"] - coord_1["y"]]
         vector_2 = [coord_3["x"] - coord_1["x"], coord_3["y"] - coord_1["y"]]
 
-        calc_area = abs((1/2)*np.cross(vector_1, vector_2))
+        calc_area = abs((1 / 2) * np.cross(vector_1, vector_2))
 
-        if calc_area > area1:       
+        if calc_area > area1:
             return True
 
     return False
+
 
 def check_lic_11() -> bool:
     # TODO: Update the function signature and implementation
     return False
 
+
 def check_lic_12() -> bool:
     # TODO: Update the function signature and implementation
     return False
+
 
 def check_lic_13() -> bool:
     # TODO: Update the function signature and implementation
     return False
 
-def check_lic_14() -> bool:
-    # TODO: Update the function signature and implementation
-    return False
 
 def check_lic_14() -> bool:
     # TODO: Update the function signature and implementation
     return False
+
+
+def check_lic_14() -> bool:
+    # TODO: Update the function signature and implementation
+    return False
+
 
 # Main function
-def get_cmv(num_points: int, points: list[Coordinate], parameters: Parameters) -> list[bool]:
+def get_cmv(
+    num_points: int, points: list[Coordinate], parameters: Parameters
+) -> list[bool]:
     return [
         check_clic_1(),
         check_lic_2(),
         check_lic_3(),
         check_lic_4(),
         check_lic_5(),
-        check_lic_6(points, parameters),
+        check_lic_6(num_points, points, parameters),
         check_lic_7(),
         check_lic_8(),
         check_lic_9(),
--- test.py
+++ test.py
@@ -6,49 +6,51 @@ from modules.types import Connectors
 from modules.cmv import *
 from snapshottest import TestCase
 
+
 class MainTests(TestCase):
     def test_should_parse_correctly(self):
-        output = parse_input('test_valid.txt')
-        self.assertMatchSnapshot(output, 'Correct inputs should be parsed correctly.')
+        output = parse_input("test_valid.txt")
+        self.assertMatchSnapshot(output, "Correct inputs should be parsed correctly.")
 
     def test_should_raise_error_if_numpoints_lt_2(self):
-        self.assertRaises(ValueError, parse_input, 'test_invalid_numpoints_1.txt')
+        self.assertRaises(ValueError, parse_input, "test_invalid_numpoints_1.txt")
 
     def test_should_raise_error_if_numpoints_gt_100(self):
-        self.assertRaises(ValueError, parse_input, 'test_invalid_numpoints_2.txt')
+        self.assertRaises(ValueError, parse_input, "test_invalid_numpoints_2.txt")
 
     def test_should_raise_error_if_numpoints_not_int(self):
-        self.assertRaises(TypeError, parse_input, 'test_invalid_numpoints_3.txt')
+        self.assertRaises(TypeError, parse_input, "test_invalid_numpoints_3.txt")
 
     def test_should_raise_error_if_numpoints_not_provided(self):
-        self.assertRaises(TypeError, parse_input, 'test_invalid_numpoints_4.txt')
+        self.assertRaises(TypeError, parse_input, "test_invalid_numpoints_4.txt")
 
     def test_should_raise_error_if_point_has_invalid_type(self):
-        self.assertRaises(TypeError, parse_input, 'test_invalid_points_1.txt')
+        self.assertRaises(TypeError, parse_input, "test_invalid_points_1.txt")
 
     def test_should_raise_error_if_point_has_invalid_length(self):
-        self.assertRaises(ValueError, parse_input, 'test_invalid_points_2.txt')
+        self.assertRaises(ValueError, parse_input, "test_invalid_points_2.txt")
 
     def test_should_raise_error_if_points_mismatch_numpoints(self):
-        self.assertRaises(ValueError, parse_input, 'test_invalid_points_3.txt')
+        self.assertRaises(ValueError, parse_input, "test_invalid_points_3.txt")
 
     def test_should_raise_error_if_params_neq_19(self):
-        self.assertRaises(ValueError, parse_input, 'test_invalid_params_1.txt')
+        self.assertRaises(ValueError, parse_input, "test_invalid_params_1.txt")
 
     def test_should_raise_error_if_params_have_invalid_types(self):
-        self.assertRaises(TypeError, parse_input, 'test_invalid_params_2.txt')
+        self.assertRaises(TypeError, parse_input, "test_invalid_params_2.txt")
 
     def test_should_raise_error_if_lcm_has_invalid_types(self):
-        self.assertRaises(TypeError, parse_input, 'test_invalid_lcm_1.txt')
+        self.assertRaises(TypeError, parse_input, "test_invalid_lcm_1.txt")
 
     def test_should_raise_error_if_lcm_has_invalid_size(self):
-        self.assertRaises(ValueError, parse_input, 'test_invalid_lcm_2.txt')
+        self.assertRaises(ValueError, parse_input, "test_invalid_lcm_2.txt")
 
     def test_should_raise_error_if_puv_has_invalid_length(self):
-        self.assertRaises(ValueError, parse_input, 'test_invalid_puv_1.txt')
+        self.assertRaises(ValueError, parse_input, "test_invalid_puv_1.txt")
 
     def test_should_raise_error_if_puv_has_invalid_types(self):
-        self.assertRaises(TypeError, parse_input, 'test_invalid_puv_2.txt')
+        self.assertRaises(TypeError, parse_input, "test_invalid_puv_2.txt")
+
 
 class DecideTests(unittest.TestCase):
     def test_should_launch_if_fuv_is_all_true(self):
@@ -67,67 +69,130 @@ class DecideTests(unittest.TestCase):
         launch = determine_launch(some_fuv)
         self.assertEqual(launch, False)
 
+
 class CMVTests(unittest.TestCase):
 
+    def test_lic_6_should_fail_if_points_are_close_to_line(self):
+        parameters = {"dist": 1, "n_pts": 3}
+        points = [
+            {"x": 0, "y": 0},
+            {"x": 1, "y": 1},
+            {"x": 4, "y": 0},
+            {"x": 5, "y": 1},
+        ]
+        num_points = len(points)
+        self.assertFalse(
+            check_lic_6(num_points, points, parameters),
+            "LIC 6: Points that are compared with dist will be close to the two created lines",
+        )
+
+    def test_lic_6_should_pass_if_points_are_far_away_from_line(self):
+        parameters = {"dist": 2, "n_pts": 3}
+        points = [{"x": 0, "y": 0}, {"x": 2, "y": 4}, {"x": 4, "y": 0}]
+        num_points = len(points)
+        self.assertTrue(
+            check_lic_6(num_points, points, parameters),
+            "LIC 6: The point compared with dist will be far away from the line",
+        )
+
+    def test_lic_6_should_pass_if_point_is_far_away_from_line_point(self):
+        parameters = {"dist": 2, "n_pts": 3}
+        points = [{"x": 0, "y": 0}, {"x": 2, "y": 2}, {"x": 0, "y": 0}]
+        num_points = len(points)
+        self.assertTrue(
+            check_lic_6(num_points, points, parameters),
+            "LIC 6: The point compared with dist will be far away from the line-point, the two points for the line are the same",
+        )
+
+    def test_lic_6_should_fail_if_input_is_illegal(self):
+        parameters = {"dist": -4, "n_pts": 2}
+        points = [{"x": 0, "y": 0}, {"x": 2, "y": 2}]
+        num_points = len(points)
+        self.assertFalse(
+            check_lic_6(num_points, points, parameters), "LIC 6: Illegal input"
+        )
+
     def test_lic_10_should_pass_if_area_gt_area1(self):
-        parameters = {
-            "area1": 1,
-            "e_pts": 3,
-            "f_pts": 4
-        }
-        points_1 = [{'x':0, 'y':0},{'x':0, 'y':0},{'x':0, 'y':0},{'x':0, 'y':0},
-                    {'x':0, 'y':0},{'x':0, 'y':0},{'x':3, 'y':0},{'x':0, 'y':0},
-                    {'x':0, 'y':0},{'x':0, 'y':0},{'x':0, 'y':0},{'x':3, 'y':3}]
+        parameters = {"area1": 1, "e_pts": 3, "f_pts": 4}
+        points_1 = [
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 3, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 3, "y": 3},
+        ]
         self.assertTrue(check_lic_10(points_1, parameters))
 
     def test_lic_10_should_fail_if_area_lte_area1(self):
-        parameters = {
-            "area1": 2,
-            "e_pts": 3,
-            "f_pts": 4
-        }
-        points_1 = [{'x':0, 'y':0},{'x':0, 'y':0},{'x':0, 'y':0},{'x':0, 'y':0},
-                    {'x':0, 'y':0},{'x':0, 'y':0},{'x':3, 'y':0},{'x':0, 'y':0},
-                    {'x':0, 'y':0},{'x':0, 'y':0},{'x':0, 'y':0},{'x':3, 'y':3}]
+        parameters = {"area1": 2, "e_pts": 3, "f_pts": 4}
+        points_1 = [
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 3, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 0, "y": 0},
+            {"x": 3, "y": 3},
+        ]
         self.assertTrue(check_lic_10(points_1, parameters))
 
     def test_lic_10_should_raise_error_if_epts_is_invalid(self):
         with self.assertRaises(ValueError):
-            points_1 = [{'x':0, 'y':0},{'x':0, 'y':0},{'x':0, 'y':0},
-                        {'x':0, 'y':0},{'x':0, 'y':0},{'x':0, 'y':0}]
-            parameters = {
-            "area1": 2,
-            "e_pts": 0,
-            "f_pts": 4
-            }   
+            points_1 = [
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+            ]
+            parameters = {"area1": 2, "e_pts": 0, "f_pts": 4}
             check_lic_10(points_1, parameters)
-    
+
     def test_lic_10_should_raise_error_if_fpts_is_invalid(self):
         with self.assertRaises(ValueError):
-            points_1 = [{'x':0, 'y':0},{'x':0, 'y':0},{'x':0, 'y':0},
-                        {'x':0, 'y':0},{'x':0, 'y':0},{'x':0, 'y':0}]
-            parameters = {
-            "area1": 2,
-            "e_pts": 3,
-            "f_pts": 0
-            }   
+            points_1 = [
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+            ]
+            parameters = {"area1": 2, "e_pts": 3, "f_pts": 0}
             check_lic_10(points_1, parameters)
 
     def test_lic_10_should_raise_error_if_epts_fpts_and_points_mismatch(self):
         with self.assertRaises(ValueError):
-            points_1 = [{'x':0, 'y':0},{'x':0, 'y':0},{'x':0, 'y':0},
-                        {'x':0, 'y':0},{'x':0, 'y':0},{'x':0, 'y':0}]
-            parameters = {
-            "area1": 2,
-            "e_pts": 8,
-            "f_pts": 4
-            }   
+            points_1 = [
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+                {"x": 0, "y": 0},
+            ]
+            parameters = {"area1": 2, "e_pts": 8, "f_pts": 4}
             check_lic_10(points_1, parameters)
+
 
 class FUVTest(unittest.TestCase):
     # TODO: Add actual tests here
     def test_something(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual("foo".upper(), "FOO")
+
 
 class PUMTest(unittest.TestCase):
     def test_pum_should_be_true_if_all_lcm_are_notused(self):
@@ -141,7 +206,9 @@ class PUMTest(unittest.TestCase):
         cmv = [False] * 15
         cmv[2] = True
         pum = get_pum(cmv=cmv, lcm=lcm)
-        expectedResult = [[(i == 2 or j == 2 or i == j) for j in range(15)] for i in range(15)]
+        expectedResult = [
+            [(i == 2 or j == 2 or i == j) for j in range(15)] for i in range(15)
+        ]
         self.assertEqual(pum, expectedResult)
 
     def test_pum_pattern_for_lcm_all_and_and_single_cmv_true(self):
@@ -153,5 +220,5 @@ class PUMTest(unittest.TestCase):
         self.assertEqual(pum, expectedResult)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
